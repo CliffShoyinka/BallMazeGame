b62863df545ef9dc5c9bc125b536ccdc27d29fde
Mehmethan Bellek <bellekmehmethan@gmail.com>
2023-05-06 00:17:34 +0300

    May 6th, 2023

--- src/Wall.java
+++ src/Wall.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Wall {
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+public class Wall extends Actor {
+
+    private Image image;
+
+
+    public Wall(int x, int y) {
+        super(x, y);
+
+        initWall();
+    }
+
+    private void initWall() {
+
+        ImageIcon icon = new ImageIcon("src/resources/wall.png");
+        image = icon.getImage();
+        setImage(image);
+    }
+
+    public void move(int x, int y) {
+
+        int dx = x() +x;
+        int dy = y() +y;
+
+        setX(dx);
+        setY(dy);
+    }
 }
